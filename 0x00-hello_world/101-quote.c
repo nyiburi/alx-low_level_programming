cde0e2efccd3170f7c1e57039c7f9f081efa3511
Markus Thomas <itsmarkust@gmail.com>
2022-10-19 18:12:36 -0700

    C program that prints exactly and that piece of art is useful - Dora Korpar  2015-10-19, followed by a new line to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
+#include <unisd.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - Prints out last part of a quote in the stadnard error.
+ * Return: 1 if success.
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that peice of art is useful  - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
+
 }

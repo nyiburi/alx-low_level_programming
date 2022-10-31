61a8b5205a0a100336d5c7a849f4b4946280f49e
Markus Thomas <itsmarkust@gmail.com>
2022-10-31 14:44:55 -0700

    Program to print ALX-Africa using _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "alx.h"
+
+/**
+ * main - Prints "ALX-Africa" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('A');
+	_putchar('L');
+	_putchar('X');
+	_putchar('-');
+	_putchar('A');
+	_putchar('f');
+	_putchar('r');
+	_putchar('i');
+	_putchar('c');
+	_putchar('a');
+	_putchar('\n');
+	return (0);
+}

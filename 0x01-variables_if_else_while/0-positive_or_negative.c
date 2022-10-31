490f43751ab971a17ace1f756f84aea2df1ac05a
Markus Thomas <itsmarkust@gmail.com>
2022-10-31 13:43:39 -0700

    Program to generate random numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,23 +1,31 @@
-#include <stdlib.h>
-#include <time.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+/*
+ * File: 0-positive_or_negative.c
+ * Auth: Brennan D Baraban
  */
 
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
+	else if (n < 0)
 		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+
 	return (0);
 }

306493c827431d30a2ccd94c58bf8dc15db998ec
Markus Thomas <itsmarkust@gmail.com>
2022-11-01 01:26:39 -0700

    Main.c and _putchar.c added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,16 +6,14 @@
  */
 int main(void)
 {
-	putchar('A');
-	putchar('L');
-	putchar('X');
-	putchar('-');
-	putchar('A');
-	putchar('f');
-	putchar('r');
-	putchar('i');
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
 	putchar('c');
+	putchar('h');
 	putchar('a');
+	putchar('r');
 	putchar('\n');
 	return (0);
 }
